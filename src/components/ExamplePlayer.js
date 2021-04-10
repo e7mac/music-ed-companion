6056415b57a7e8494290dd67a5799a3c7eafcbce
Mayank Sanganeria <mayank.ot@gmail.com>
2021-04-10 12:07:47 -0700

    warnings

--- src/components/ExamplePlayer.js
+++ src/components/ExamplePlayer.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 export default function ExamplePlayer(props) {
 	return (
     <>
         <p>
-          <img src={props.item.image}/>
+          <img src={props.item.image} alt="Musical example"/>
         </p>
         <p>
           <midi-player
